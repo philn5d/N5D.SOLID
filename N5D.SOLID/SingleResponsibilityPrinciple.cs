b2c8276975aeddbb5331a7be93c5faf7c40dd18b
philn5d <philn5d@gmail.com>
2016-10-26 08:33:24 -0500

    initial violoation of SRP

---
+++ N5D.SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace N5D.SOLID
+{
+    [TestClass]
+    public class SingleResponsibilityPrinciple
+    {
+        [TestMethod]
+        public void AClassShouldOnlyHaveOneReasonToChange()
+        {
+            //A class either does work itself, or knows other classes that do work
+        }
+
+        [TestMethod]
+        public void A_violation_of_SRP_can_expose_code_to_more_defects_and_create_more_work()
+        {
+            Violating.Person parent = new Violating.Person();
+            Violating.Person principle = new Violating.Person();
+            Violating.Person student = new Violating.Person();
+            Violating.Person teacher = new Violating.Person();
+
+            var people = new[] { parent, student, principle, teacher };
+
+            NotifyAbsences(people);
+
+            var grades = DoesMathGradeQualify(student);
+        }
+
+        private static void NotifyAbsences(Violating.Person[] people)
+        {
+            foreach (var p in people)
+            {
+                if (p.IsStudent)
+                {
+                    foreach (Violating.Person relatedPerson in p.PeopleToNotify)
+                    {
+                        switch (relatedPerson.Role)
+                        {
+                            case "Teacher":
+                                relatedPerson.NotifyStudentAbsence(p);
+                                break;
+                            case "Parent":
+                                relatedPerson.NotifyChildAbsence(p);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool DoesMathGradeQualify(Violating.Person student)
+        {
+            var grade = student.GetGrade("Math");
+
+            if (grade == null)
+                return false;
+            if (grade == "A")
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ N5D.SOLID/Violating/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Violating
+{
+    class Person
+    {
+        private Tuple<string, string>[] _grades = new Tuple<string, string>[] {
+            new Tuple<string, string>("English", "A"),
+            new Tuple<string, string>("Math", "A"),
+            new Tuple<string, string>("Computer Programming", "D")
+        };
+        public bool IsStudent { get; internal set; }
+        public IEnumerable<Person> PeopleToNotify { get; internal set; }
+        public string Role { get; internal set; }
+        public string Email { get; private set; }
+        public string Name { get; private set; }
+
+        internal void NotifyStudentAbsence(Person p)
+        {
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                string body = string.Format("{0} will be absent from class today.", p.Name);
+                MailMessage msg = new MailMessage(this.Email, p.Email, "Notification of Student Absence", body);
+                smtp.Send(msg);
+            }
+        }
+
+        internal void NotifyChildAbsence(Person p)
+        {
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                string body = string.Format("We have been notified of {0} absence today.", p.Name);
+                MailMessage msg = new MailMessage(this.Email, p.Email, "Notification of Student Absence", body);
+                smtp.Send(msg);
+            }
+        }
+
+        internal string GetGrade(string subject)
+        {
+            return _grades.SingleOrDefault(x => x.Item1 == subject)?.Item2;
+        }
+    }
+}
